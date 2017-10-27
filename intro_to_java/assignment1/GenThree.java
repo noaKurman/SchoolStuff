15de9e0de7bdda9d5d39a16210783e87f3ba5489
Noa Kurman <noa@bigpanda.io>
2017-10-27 11:11:52 +0300

    add files and moves some targilim

---
+++ intro_to_java/assignment1/GenThree.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+// This class generates three numbers in a range, get the lowest
+public class GenThree {
+    /*
+     * Assignment number : 1.1
+     * File Name : GenThree.java
+     * Name (First Last) : Noa Kurman
+     * Student ID : 204404305
+     * Email : noa.kurman@post.idc.ac.il
+     * */
+
+	public static void main(String[] args) {
+        int min = Integer.parseInt(args[0]);
+        int max = Integer.parseInt(args[1]);
+        
+        // Define random and generate three numbers in the range
+        Random rand = new Random();
+        int firstRandom = rand.nextInt(max - min) + min;
+        int secondRandom = rand.nextInt(max - min) + min;
+        int thirdRandom = rand.nextInt(max - min) + min;
+        
+        // Get the lowest number, first compare two first numbers and then the lowest vs. the third
+        int lowest = Math.min(firstRandom, secondRandom);
+        lowest = Math.min(lowest, thirdRandom);
+        
+        // Print all the generated numbers in different line, as in the example 
+        System.out.println(firstRandom);
+        System.out.println(secondRandom);
+        System.out.println(thirdRandom);
+        System.out.println("The minimal generated number was " + lowest);
+	}
+
+}
---
+++ intro_to_java/assignment1/SinFunction.java
@@ -0,0 +1,41 @@
+import static java.lang.Math.*;
+// This class shows the sinus of PI
+public class SinFunction {
+
+    /*
+     * Assignment number : 1.3
+     * File Name : SinFunction.java
+     * Name (First Last) : Noa Kurman
+     * Student ID : 204404305
+     * Email : noa.kurman@post.idc.ac.il
+     */
+
+	public static void main (String[] args){
+		
+        System.out.println("sin(0) = " + Math.sin(0*Math.PI));  
+        System.out.println("sin(1/4) = " + Math.sin(0.25*Math.PI));  
+        System.out.println("sin(1/2) = " + Math.sin(0.50*Math.PI));  
+        System.out.println("sin(3/4) = " + Math.sin(0.75*Math.PI));  
+        System.out.println("sin(PI) = " + Math.sin(1.0*Math.PI));  
+        System.out.println("sin(5/4) = " + Math.sin(1.25*Math.PI));  
+        System.out.println("sin(3/2) = " + Math.sin(1.5*Math.PI));  
+        System.out.println("sin(7/4) = " + Math.sin(1.75*Math.PI));  
+        System.out.println("sin(2 PI) = " + Math.sin(2.0*Math.PI));  
+        
+        /* Dear bodek, in the following comment i added a part to calculate the sin using a for loop, 
+         * i already wrote it and felt bad to throw it away, so its here. 
+         */
+        
+        //for(double i=0.0; i<=2.0; i+=1d/4d){
+        //    double sin = Math.sin(i*Math.PI);
+        //    if(i > 0){
+        //      int firstNum = (int)Math.floor((i*100)/25);
+        //      int secondNum = (100 / 25);
+        //      System.out.println("sin(" + firstNum + "/" + secondNum + " PI) = " + sin);
+        //    }
+        //    else{
+        //      System.out.println("sin(" + i + ") = " + sin);
+        //    }
+        //}	
+	}	
+}
